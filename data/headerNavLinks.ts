85d0b53ccfa1c2a8e72ed7ec8bb4d786ba92b43a
Owais Abbasi <owaisabbasi@localhost.localdomain>
2024-08-19 15:38:39 -0400

    updated About me page

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -3,7 +3,7 @@ const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
   //  { href: '/projects', title: 'Projects' },
-  // { href: '/about', title: 'About Me' },
+  { href: '/about', title: 'About Me' },
   { href: 'resume.pdf', title: 'Resume', external: true },
 ]
 
