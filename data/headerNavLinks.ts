38893fa58d4da2a2018c69f7973a28b81b4f744a
Owais Abbasi <owaisabbasi@localhost.localdomain>
2024-08-19 08:10:49 -0400

    updated blog

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,9 +1,10 @@
 const headerNavLinks = [
-  { href: '/', title: 'Home' },
+  //  { href: '/', title: 'Home' },
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
-  { href: '/about', title: 'About' },
+  //  { href: '/projects', title: 'Projects' },
+  // { href: '/about', title: 'About Me' },
+  { href: 'resume.pdf', title: 'Resume', external: true },
 ]
 
 export default headerNavLinks
