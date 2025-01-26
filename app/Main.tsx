1d46e8fa6f901f7511878af9938ecdaef930aac6
owais-io <owais.abbasi9@gmail.com>
2025-01-26 10:16:19 +0300

    modified front page text

--- app/Main.tsx
+++ app/Main.tsx
@@ -5,7 +5,7 @@ import siteMetadata from '@/data/siteMetadata'
 import { formatDate } from 'pliny/utils/formatDate'
 // import NewsletterForm from 'pliny/ui/NewsletterForm'
 
-const MAX_DISPLAY = 20
+const MAX_DISPLAY = 5
 
 export default function Home({ posts }) {
   return (
@@ -26,8 +26,8 @@ export default function Home({ posts }) {
                 className="rounded-full"
               />
             </div>
-            <div className="md:w-2/3">
-              <p className="text-lg leading-7 text-gray-500 dark:text-gray-400 text-justify">&nbsp;&nbsp;&nbsp;&nbsp;As a DevOps Engineer with a passion for automation, cloud-native tools, and secure infrastructure, I bring a unique perspective shaped by my transition from a finance and accounting background. Currently pursuing a world-class Level 6 Diploma in Artificial Intelligence Operations (AIOps), I am upskilling myself with cutting-edge expertise in DevOps, Cloud Security, and AI-driven cloud solutions. My hands-on experience spans Linux, Docker, Jenkins, Ansible, Terraform, AWS, Bash and Python, coupled with a proactive approach to learning Kubernetes and integrating security best practices.<br /><br />&nbsp;&nbsp;&nbsp;&nbsp;Being new to this field, I come without a rigid mindset, making me adaptable and ready to evolve according to organizational needs. This flexibility, combined with my growing knowledge in Cybersecurity (a key component of my diploma), positions me as a future-ready DevSecOps Engineer who prioritizes secure and efficient infrastructure. My earlier experience in accounting and data roles has honed my attention to detail and organizational skills, further enriching my capabilities. I believe in continuous growth and am excited to contribute to your team’s success while building innovative, secure solutions.</p>
+            <div className="md:w-2/3 text-lg leading-7 text-gray-500 dark:text-gray-400 text-justify">
+              As a DevOps Engineer.
             </div>
           </div>
         </div>
