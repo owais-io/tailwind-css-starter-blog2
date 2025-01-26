02e66aac94c3e892e4289aa6d59097fead02dcf3
owais-io <owais.abbasi9@gmail.com>
2025-01-26 08:58:24 +0300

    modified bio descripted in main.tsx

--- app/Main.tsx
+++ app/Main.tsx
@@ -4,7 +4,6 @@ import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import { formatDate } from 'pliny/utils/formatDate'
 // import NewsletterForm from 'pliny/ui/NewsletterForm'
-// import AboutContent from './about.mdx'
 import { AboutSection } from '@/components/AboutSection'
 
 const MAX_DISPLAY = 5
@@ -30,13 +29,6 @@ export default function Home({ posts }) {
             </div>
             <div className="md:w-2/3">
             <AboutSection />
-            {/* <div className="prose max-w-none text-gray-500 dark:text-gray-400">
-  <AboutContent />
-</div> */}
-            {/* <p className="text-lg leading-7 text-gray-500 dark:text-gray-400 text-justify">&nbsp;&nbsp;&nbsp;&nbsp;As a DevOps Engineer with a passion for automation, cloud-native tools, and secure infrastructure, I bring a unique perspective shaped by my transition from a finance and accounting background. Currently pursuing a world-class Level 6 Diploma in Artificial Intelligence Operations (AIOps), I am upskilling myself with cutting-edge expertise in DevOps, Cloud Security, and AI-driven cloud solutions. My hands-on experience spans Linux, Docker, Jenkins, Ansible, Terraform, AWS, Bash and Python, coupled with a proactive approach to learning Kubernetes and integrating security best practices.</p>
-
-<p className="text-lg leading-7 text-gray-500 dark:text-gray-400 text-justify">&nbsp;&nbsp;&nbsp;&nbsp;Being new to this field, I come without a rigid mindset, making me adaptable and ready to evolve according to organizational needs. This flexibility, combined with my growing knowledge in Cybersecurity (a key component of my diploma), positions me as a future-ready DevSecOps Engineer who prioritizes secure and efficient infrastructure. My earlier experience in accounting and data roles has honed my attention to detail and organizational skills, further enriching my capabilities. I believe in continuous growth and am excited to contribute to your team’s success while building innovative, secure solutions.</p>
- */}
             </div>
           </div>
         </div>
--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -7,9 +7,3 @@ export function AboutSection() {
       </>
     )
   }
-  
-
-  {/* <p className="text-lg leading-7 text-gray-500 dark:text-gray-400 text-justify">&nbsp;&nbsp;&nbsp;&nbsp;As a DevOps Engineer with a passion for automation, cloud-native tools, and secure infrastructure, I bring a unique perspective shaped by my transition from a finance and accounting background. Currently pursuing a world-class Level 6 Diploma in Artificial Intelligence Operations (AIOps), I am upskilling myself with cutting-edge expertise in DevOps, Cloud Security, and AI-driven cloud solutions. My hands-on experience spans Linux, Docker, Jenkins, Ansible, Terraform, AWS, Bash and Python, coupled with a proactive approach to learning Kubernetes and integrating security best practices.</p>
-
-<p className="text-lg leading-7 text-gray-500 dark:text-gray-400 text-justify">&nbsp;&nbsp;&nbsp;&nbsp;Being new to this field, I come without a rigid mindset, making me adaptable and ready to evolve according to organizational needs. This flexibility, combined with my growing knowledge in Cybersecurity (a key component of my diploma), positions me as a future-ready DevSecOps Engineer who prioritizes secure and efficient infrastructure. My earlier experience in accounting and data roles has honed my attention to detail and organizational skills, further enriching my capabilities. I believe in continuous growth and am excited to contribute to your team’s success while building innovative, secure solutions.</p>
- */}
